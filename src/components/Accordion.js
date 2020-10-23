3d7f5dd6e33c8b1ddaeb0f483934d6a261f6c26c
Alberto Sánchez <albertoensan@gmail.com>
2020-10-23 02:04:50 +0200

    add useState and minor changes in ui

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const onTitleClick = (index) => {
+        //console.log('Title clicked', index);
+        setActiveIndex(index);
+    };
+
     const renderedItems = items.map((item, index) => {
+        const active = index === activeIndex ? 'active' : '';
+
         return (
             <React.Fragment key={item.title} >
                 <div
-                    className="title active"
-                    onClick={() => console.log('Title clicked', index)} >
+                    className={`title ${active}`}
+                    onClick={() => onTitleClick(index)}
+                >
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
-                <div className="content active">
+                <div className={`content ${active}`}>
                     <p>{item.content}</p>
                 </div>
             </React.Fragment>
